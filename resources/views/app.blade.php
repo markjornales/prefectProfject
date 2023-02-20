9cd7ebf5b8dcab21aa0a2ce3802788a5eec824a9
markjoseph <markjornales@gmail.com>
2023-02-21 02:16:02 +0800

    new build changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
-        <link rel="stylesheet" href="{{asset('assets/css/loading.css')}}" />
+        <link rel="stylesheet" href="/public/assets/css/main.css" />
+        <link rel="stylesheet" href="/public/assets/css/loading.css" />
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
