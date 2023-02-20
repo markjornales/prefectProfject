1ccd1b5661adf3b9513f950aac160be37ab5fac7
markjoseph <markjornales@gmail.com>
2023-02-20 23:46:49 +0800

    changes

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,7 @@ use App\Http\Controllers\Main\HomeController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-
-a
-sdas
+ 
 
 */
 
