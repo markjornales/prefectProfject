e20f11dbcdc725eb646fbc1f681d55438b60eb91
markjoseph <markjornales@gmail.com>
2023-02-20 23:42:07 +0800

    changes

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,10 @@ use App\Http\Controllers\Main\HomeController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
+
+a
+sdas
+
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
